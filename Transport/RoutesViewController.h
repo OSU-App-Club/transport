9453f4ff5919ff55d9ea45a8938b2589019be35b
Bret Lorimore <bretlorimore@10-196-58-173.wireless.oregonstate.edu>
2014-04-19 12:07:16 -0700

    made routes view controller.
    working on adding view to cell to display data - not quite there yet

---
+++ Transport/RoutesViewController.h
@@ -0,0 +1,13 @@
+//
+//  RoutesViewController.h
+//  Transport
+//
+//  Created by Bret Lorimore on 4/19/14.
+//  Copyright (c) 2014 OSU App Club. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RoutesViewController : UIViewController
+
+@end
---
+++ Transport/RoutesViewController.m
@@ -0,0 +1,49 @@
+//
+//  RoutesViewController.m
+//  Transport
+//
+//  Created by Bret Lorimore on 4/19/14.
+//  Copyright (c) 2014 OSU App Club. All rights reserved.
+//
+
+#import "RoutesViewController.h"
+
+@interface RoutesViewController ()
+
+@end
+
+@implementation RoutesViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
