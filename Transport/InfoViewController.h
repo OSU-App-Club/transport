632b2bba99246b248c28491e48d50e149057c2d6
cvanderschuere <cdvconcepts@gmail.com>
2014-04-19 18:07:56 -0700

    Added info screen

---
+++ Transport/InfoViewController.h
@@ -0,0 +1,15 @@
+//
+//  InfoViewController.h
+//  Transport
+//
+//  Created by Chris Vanderschuere on 4/19/14.
+//  Copyright (c) 2014 OSU App Club. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfoViewController : UIViewController
+
+- (IBAction) closeInfoScreen:(id)sender;
+
+@end
---
+++ Transport/InfoViewController.m
@@ -0,0 +1,53 @@
+//
+//  InfoViewController.m
+//  Transport
+//
+//  Created by Chris Vanderschuere on 4/19/14.
+//  Copyright (c) 2014 OSU App Club. All rights reserved.
+//
+
+#import "InfoViewController.h"
+
+@interface InfoViewController ()
+
+@end
+
+@implementation InfoViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)closeInfoScreen:(id)sender{
+    [self.parentViewController dismissViewControllerAnimated:YES completion:nil];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
